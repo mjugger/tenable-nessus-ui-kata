44a41db73a14cc2fc801291afa0080e76beef9d9
Mukhtar Jugger <xaiox20@gmail.com>
2016-10-16 20:52:05 -0400

    updated returned json

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var express = require('express')();
 var path = require('path');
+var faker = require('faker');
+var fakeDatabase = [];
+
+(function generateFakeDB(dbSize){
+	for (var i = 0; i < dbSize; i++) {
+		fakeDatabase.push({
+			name:faker.name.findName(),
+			hostname:faker.internet.url(),
+			port:faker.random.number(),
+			username:faker.internet.userName()
+		});
+	};
+})(10000);
+
 express.set('views', path.join(__dirname, 'views'));
 express.set('view engine', 'jade');
 
 express.get('/download/request',function(req,res){
-	res.status(200).json({message:req.query.host});
+	res.status(200).json({configurations:fakeDatabase});
 });
 
 express.listen(3005,function(){
