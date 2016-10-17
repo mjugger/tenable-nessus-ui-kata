7f0c4e2ca7654b431553e4b4f187a5b5d49f494f
Mukhtar Jugger <xaiox20@gmail.com>
2016-10-17 19:19:58 -0400

    updated gulp file tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,29 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var cleanMinify = require('gulp-clean-css');
+var stylus = require('gulp-stylus');
+var mainBowerFiles = require('gulp-main-bower-files');
+var uglify = require('gulp-uglify');
+
+
+gulp.task('bowerStyles',function(){
+	return gulp.src('bower.json')
+	.pipe( mainBowerFiles('**/*.css') )
+	.pipe( concat('helperStyles.css') )
+	.pipe( cleanMinify() )
+	.pipe( gulp.dest('build/css') );
+});
+
+gulp.task('scripts',function(){
+	return gulp.src('source/scripts/*.js')
+		.pipe(concat('all.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('public/js'));
+});
+
+gulp.task('watch',function(){
+	gulp.watch('source/styles/*.styl',['bowerStyles']);
+	gulp.watch('source/scripts/*.js',['scripts']);
+});
+
+gulp.task('default',['bowerStyles','scripts','watch']);
