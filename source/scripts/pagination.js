3ec4e18dc4deeda3997d7b3ac16360b94c8cff76
Mukhtar Jugger <xaiox20@gmail.com>
2016-10-19 17:27:56 -0400

    updated code comments

--- source/scripts/pagination.js
+++ source/scripts/pagination.js
@@ -1,5 +1,16 @@
 (function(){
 	'use strict'
+
+	/**
+	 * [Constructor: the contructor method for this app]
+	 * @param {[Number]} perPageLimit         [the amount of records to show for each page]
+	 * @param {[String]} url                  [the restful API url used to retrieved the data]
+	 * @param {[Number]} recordAmount         [the amount of records to return]
+	 * @param {[String]} pageSelectionID      [the element selector used to place the option elements]
+	 * @param {[String]} injectionContainerID [the element selector used to place records in the DOM]
+	 * @param {[String]} searchFormID         [the element selector of the form containing the seach field]
+	 * @param {[String]} searchFieldID        [the element selector of the search field]
+	 */
 	function Constructor(perPageLimit,url,recordAmount,pageSelectionID,injectionContainerID,searchFormID,searchFieldID){
 		if(!(this instanceof Constructor) ){
 			throw "pagination.js must be constructed with \"new\" keyword";
@@ -15,6 +26,9 @@
 		this.data = null;
 	}
 
+	/**
+	 * [create: initializes the app]
+	 */
 	Constructor.prototype.create = function(){
 		var me = this;
 		me.fetchData(function(data){
@@ -26,6 +40,9 @@
 		});
 	}
 
+	/**
+	 * [setUpSearch: initilizes the search feature]
+	 */
 	Constructor.prototype.setUpSearch = function(){
 		if(this.searchForm && searchField){
 			var me = this;
@@ -39,6 +56,9 @@
 		}
 	}
 
+	/**
+	 * [setUpPageSelector: initializes the page selection feature]
+	 */
 	Constructor.prototype.setUpPageSelector = function(){
 		var me = this;
 		if(this.pageSelector){
@@ -50,6 +70,10 @@
 		}
 	}
 
+	/**
+	 * [setUpPageContent: swaps page content out for new conent]
+	 * @param {[Number]} pageNumber [the total number of pages]
+	 */
 	Constructor.prototype.setUpPageContent = function(pageNumber){
 		if(this.injectionContainer.hasChildNodes){
 			this.removePageContent();
@@ -59,16 +83,27 @@
 		this.addPageContent(DOMElements);
 	}
 
+	/**
+	 * [removePageContent: removes all content from the injection container]
+	 */
 	Constructor.prototype.removePageContent = function(){
 		while(this.injectionContainer.hasChildNodes()){
 			this.injectionContainer.removeChild(this.injectionContainer.lastChild);
 		}
 	}
 
+	/**
+	 * [addPageContent: places content contained in a document fragment into the injection container]
+	 * @param {[Array]} pageContent [an Array of object literals containing the page content]
+	 */
 	Constructor.prototype.addPageContent = function(pageContent){
 		this.injectionContainer.appendChild(pageContent);
 	}
 
+	/**
+	 * [fetchData: fetches the json data from the server's restful API]
+	 * @param  {Function} callback [function to call when operation is successful]
+	 */
 	Constructor.prototype.fetchData = function(callback){
 		var xhr = new XMLHttpRequest();
 		xhr.open('get', this.url+this.recordAmount, true);
@@ -82,14 +117,29 @@
 		xhr.send();
 	}
 
+	/**
+	 * [isFloat: determains if a number is a floating point number]
+	 * @param  {[Number]}  num [the number to check]
+	 * @return {Boolean}     [true if the number is floating point and false if it is not]
+	 */
 	Constructor.prototype.isFloat = function(num){
 		return Boolean(num%1);
 	}
 
+	/**
+	 * [isOdd: determains if number is odd]
+	 * @param  {[Number]}  number [the number to check]
+	 * @return {Boolean}        [true if the number is odd and false if it is even]
+	 */
 	Constructor.prototype.isOdd = function(number){
 		return Boolean(number%2);
 	}
 
+	/**
+	 * [calculateTotalPages: calculates the total number of pages to generate based on the per page limit]
+	 * @param  {[Array]} dataArray [the array of object literals to calculate]
+	 * @return {[Number]}           [the total number of pages]
+	 */
 	Constructor.prototype.calculateTotalPages = function(dataArray){
 		var recordCount = dataArray.length;
 		var pageCount = recordCount / this.perPageLimit;
@@ -99,12 +149,22 @@
 		return pageCount;
 	}
 
+	/**
+	 * [getPageData: copies a peice of the data array based on the page the sata belongs to]
+	 * @param  {[Number]} pageToReturn [the page the data will live on]
+	 * @return {[Array]}              [the copy of the data for the specified page]
+	 */
 	Constructor.prototype.getPageData = function(pageToReturn){
 		var startingIndex = (pageToReturn * this.perPageLimit);
 		var endingIndex = startingIndex + this.perPageLimit;
 		return this.data.slice(startingIndex,endingIndex);
 	}
 
+	/**
+	 * [getSearchData: serches the data array for objects matching the search criteria]
+	 * @param  {[String]} searchTerm [the value to search for]
+	 * @return {[Array]}            [an array containing the results of the search]
+	 */
 	Constructor.prototype.getSearchData = function(searchTerm){
 		var results = [];
 		var match;
@@ -124,6 +184,11 @@
 		return results;
 	}
 
+	/**
+	 * [createPageSelections: creates the option elements for the page selection dropdown]
+	 * @param  {[Number]} numberOfPages [the amount of option elements to create based on the number of pages]
+	 * @return {[Object]}               [a DocumentFragment containing all the option elements]
+	 */
 	Constructor.prototype.createPageSelections = function(numberOfPages){
 		var docFrag = document.createDocumentFragment();
 		var selectEl;
@@ -136,6 +201,11 @@
 		return docFrag;
 	}
 
+	/**
+	 * [createPageElements: creates an HTML structure for the page data]
+	 * @param  {[Array]} pageData [the data used to create the HTML markup]
+	 * @return {[Object]}          [a DocumentFragment containing all the card/div elements]
+	 */
 	Constructor.prototype.createPageElements = function(pageData){
 		var docFrag = document.createDocumentFragment();
 		var container;
