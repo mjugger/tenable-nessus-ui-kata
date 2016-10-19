da9491347bfcc7ee05de8604470753c901f11230
Mukhtar Jugger <xaiox20@gmail.com>
2016-10-19 02:17:43 -0400

    parse content and page selectors and creates their UI

--- source/scripts/pagination.js
+++ source/scripts/pagination.js
@@ -1,25 +1,61 @@
 (function(){
 	'use strict'
-	function Constructor(perPageLimit,url,recordAmount){
+	function Constructor(perPageLimit,url,recordAmount,pageSelectionID,injectionContainerID){
 		if(!(this instanceof Constructor) ){
 			throw "pagination.js must be constructed with \"new\" keyword";
 		}
 		this.perPageLimit = perPageLimit;
 		this.url = url;
 		this.recordAmount = recordAmount;
-		this.data;
+		this.pageSelector = document.querySelector(pageSelectionID);
+		this.injectionContainer = document.querySelector(injectionContainerID);
+		this.pageCount = null;
+		this.data = null;
 	}
 
-	Constructor.prototype.init = function(){
+	Constructor.prototype.create = function(){
 		var me = this;
 		me.fetchData(function(data){
-			me.data = data;
+			me.data = data.configurations;
+			me.pageCount = me.calculateTotalPages(me.data);
+			me.setUpPageSelector();
+			me.setUpPageContent(0);
+			console.log('data: ',me.data,' pagecount: ',me.pageCount);
 		});
+	}
+
+	Constructor.prototype.setUpPageSelector = function(){
+		var me = this;
+		if(this.pageSelector){
+			var pageNumbers = this.createPageSelections(this.pageCount);
+			this.pageSelector.appendChild(pageNumbers);
+			this.pageSelector.addEventListener('change',function(){
+				me.setUpPageContent(parseInt(this.selectedIndex));
+			});
+		}
+	}
+
+	Constructor.prototype.setUpPageContent = function(pageNumber){
+		if(this.injectionContainer.hasChildNodes){
+			this.removePageContent();
+		}
+		this.addPageContent(pageNumber);
+	}
+
+	Constructor.prototype.removePageContent = function(){
+		while(this.injectionContainer.hasChildNodes()){
+			this.injectionContainer.removeChild(this.injectionContainer.lastChild);
+		}
+	}
+
+	Constructor.prototype.addPageContent = function(pageNumber){
+		var pageData = this.getPageData(pageNumber);
+		this.injectionContainer.appendChild(this.createPageElements(pageData));
 	}
 
 	Constructor.prototype.fetchData = function(callback){
 		var xhr = new XMLHttpRequest();
-		xhr.open('get', this.url+me.recordAmount, true);
+		xhr.open('get', this.url+this.recordAmount, true);
 		xhr.responseType = 'json';
 		xhr.onload = function(){
 			var status = xhr.status;
@@ -27,6 +63,7 @@
 				callback(xhr.response);
 			}
 		}
+		xhr.send();
 	}
 
 	Constructor.prototype.isFloat = function(num){
@@ -46,6 +83,42 @@
 		var startingIndex = (pageToReturn * this.perPageLimit);
 		var endingIndex = startingIndex + this.perPageLimit;
 		return this.data.slice(startingIndex,endingIndex);
+	}
+
+	Constructor.prototype.createPageSelections = function(numberOfPages){
+		var docFrag = document.createDocumentFragment();
+		var selectEl;
+		for (var i = 0; i <= numberOfPages; i++) {
+			selectEl = document.createElement('option');
+			selectEl.textContent = 'page: '+(i+1);
+			docFrag.appendChild(selectEl);
+		};
+		return docFrag;
+	}
+
+	Constructor.prototype.createPageElements = function(pageData){
+		var docFrag = document.createDocumentFragment();
+		var container;
+		var title;
+		var h2;
+		var cardBody;
+		for (var i = 0; i < pageData.length; i++) {
+			container = document.createElement('div');
+			title = document.createElement('div');
+			h2 = document.createElement('h2');
+			cardBody = document.createElement('div');
+			container.className = 'mdl-card mdl-shadow--4dp';
+			title.className = 'mdl-card__title';
+			h2.className = 'mdl-card__title-text';
+			cardBody.className = 'mdl-card__supporting-text';
+			h2.textContent = pageData[i].name;
+			cardBody.innerHTML = pageData[i].hostname+'<br/>'+pageData[i].port+'<br/>'+pageData[i].username;
+			title.appendChild(h2);
+			container.appendChild(title);
+			container.appendChild(cardBody);
+			docFrag.appendChild(container);
+		};
+		return docFrag;
 	}
 
 	if(!window.pagination){
